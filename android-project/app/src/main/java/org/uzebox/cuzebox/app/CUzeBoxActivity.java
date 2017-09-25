7773540ad70f29dba4b64daa87cd282ecb83e90e
Matt Pandina <artcfox@gmail.com>
2017-09-25 12:10:26 -0400

    Add support for Runtime Permissions, so targetSdkVersion can be the latest

--- android-project/app/src/main/java/org/uzebox/cuzebox/app/CUzeBoxActivity.java
+++ android-project/app/src/main/java/org/uzebox/cuzebox/app/CUzeBoxActivity.java
@@ -1,9 +1,64 @@
 package org.uzebox.cuzebox.app;
 
-import org.libsdl.app.SDLActivity; 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
 
 /**
- * A sample wrapper class that just calls SDLActivity 
- */ 
+ * Handles requesting Runtime Permissions when running on Marshmallow or newer.
+ */
 
-public class CUzeBoxActivity extends SDLActivity { }
+public class CUzeBoxActivity extends Activity {
+    public static final int REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION = 1337;
+
+    private void LaunchNextActivity() {
+        Intent intent = new Intent(this, LaunchApp.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            LaunchNextActivity();
+        } else {
+            AlertDialog.Builder builder;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                builder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
+            } else {
+                builder = new AlertDialog.Builder(this);
+            }
+            builder.setTitle(R.string.no_permission_title)
+                   .setMessage(R.string.no_permission_message)
+                   .setCancelable(false)
+                   .setPositiveButton(R.string.no_permission_button_text, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                   })
+                   .show();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (this.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                this.requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION);
+            } else {
+                LaunchNextActivity();
+            }
+        } else {
+            LaunchNextActivity();
+        }
+    }
+
+}
---
+++ android-project/app/src/main/java/org/uzebox/cuzebox/app/LaunchApp.java
@@ -0,0 +1,9 @@
+package org.uzebox.cuzebox.app;
+
+import org.libsdl.app.SDLActivity;
+
+/**
+ * A sample wrapper class that just calls SDLActivity
+ */
+
+public class LaunchApp extends SDLActivity { }
